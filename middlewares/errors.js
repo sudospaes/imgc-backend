f1011810a6c0eb24426cfa34ecb9b344d5eb1f6e
Sudo Space <abolfazleffati1381@gmail.com>
2023-09-09 21:39:08 +0330

    Clean

--- middlewares/errors.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message, data });
-};
-
-module.exports = { errorHandler };
