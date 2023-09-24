e304344391bc6023a4fd04da687413a8f6b0040e
SudoSpace <abolfazleffati1381@gmail.com>
2023-09-25 00:13:30 +0330

    improve

--- src/models/validattion.ts
+++ src/models/validattion.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const imageSchema = yup.object().shape({
   name: yup.string().required("Image should have a name"),
-  size: yup.number().max(10000000, "Image can't be larger of 10MB"),
+  size: yup.number().max(50000000, "Image can't be larger of 50MB"),
   mimetype: yup
     .mixed()
     .oneOf(["image/jpeg", "image/png"], "Please upload only .jpeg/jpg or .png"),
