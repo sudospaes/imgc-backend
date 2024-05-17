777927e62121fd0de76721542fe7b84664693a18
Sudo Space <79229394+sudospaes@users.noreply.github.com>
2024-05-17 16:02:23 +0330

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,7 +5,7 @@ import controller from "../controllers/controller";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  res.status(201).json({ message: "Hello my sweetie" });
 });
 
 router.get("/:imageName", controller.getImage);
